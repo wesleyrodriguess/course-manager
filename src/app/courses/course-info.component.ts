26ab3ed910dcaea106b8d7f1ac709c07f6afab1f
Wesley Rodrigues <wesleysilvarodrigues25@gmail.com>
2021-09-06 20:43:14 -0300

    feat:add course info

---
+++ src/app/courses/course-info.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+    templateUrl: './course-info.component.html'
+})
+export class CourseInfoComponent {
+
+    courseId: number;
+
+    constructor(private activatedRoute: ActivatedRoute) {}
+
+    ngOnInit(): void {
+        this.courseId = +this.activatedRoute.snapshot.paramMap.get('id');
+    }
+}
